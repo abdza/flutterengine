c62b6e5effcbf9721abfec36084437315b3db64e
abdza <abdullah.zainul@gmail.com>
2019-02-12 07:51:55 +0800

    Detail will also get from the server

--- lib/recordlist.dart
+++ lib/recordlist.dart
@@ -4,39 +4,43 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import './recordview.dart';
 
-final serverbase = "http://10.132.1.135:8800";
+var serverbase = '';
+
+final storage = new FlutterSecureStorage();
 
 class RecordListWidget extends StatefulWidget {
+  generalsetup() async {
+    serverbase = await storage.read(key: 'serverbase');
+  }
+
+  RecordListWidget() {
+    generalsetup();
+  }
+
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return RecordListState();
   }
 }
 
 class RecordListState extends State<RecordListWidget> {
-
   var _isLoading = true;
 
   var _emailvalue = null;
   var _passwordvalue = null;
 
-  final _storage = new FlutterSecureStorage();
-
   var items = [];
-    /* {'name':'Namaku','desc':'Desc ku'},
-    {'name':'Namaku 2','desc':'Desc ku 2'},
-    {'name':'Namaku 3','desc':'Desc ku 3'}
-  ];*/
+  var fields = [];
 
   _setlocaldata() async {
     print("setting localdata");
-    _emailvalue = await _storage.read(key:'email');
-    _passwordvalue = await _storage.read(key:'password');
+    _emailvalue = await storage.read(key: 'email');
+    _passwordvalue = await storage.read(key: 'password');
     print("after localdata for $_emailvalue and $_passwordvalue");
     _fetchData();
   }
 
-  RecordListState(){
+  RecordListState() {
     print("init list");
     _setlocaldata();
   }
@@ -44,50 +48,47 @@ class RecordListState extends State<RecordListWidget> {
   _fetchData() async {
     print("in fetch for $_emailvalue and $_passwordvalue");
     Dio dio = new Dio(new BaseOptions(headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization":
-            "Basic " + base64Encode(utf8.encode('$_emailvalue:$_passwordvalue'))
-      }));
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization":
+          "Basic " + base64Encode(utf8.encode('$_emailvalue:$_passwordvalue'))
+    }));
     final url = serverbase + "/system/participants/jeager_2h_2918";
     final response = await dio.get(url);
     print("Fetching data");
 
-    if(response.statusCode==200){
+    if (response.statusCode == 200) {
       final map = response.data;
       print("map:" + map.toString());
       print("----------------------------------------------------------------");
       print("records:" + map['records'].toString());
-      final assignmentsjson = map['records'];
-      setState((){
+      setState(() {
         _isLoading = false;
-        this.items = assignmentsjson;
+        this.items = map['records'];
+        this.fields = map['listfields'];
       });
     }
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-                title: Text('Flutter Engine')
-            ),
-            body: Center(
-                child: ListView.builder(
-                  itemCount: this.items !=null ? this.items.length : 0,
-                  itemBuilder: (context, i){
-                    final curitem = this.items[i];
-                    return ListTile(
-                      title: Text(curitem['participant_list']),
-                      subtitle: Text(curitem['staff_no']),
-                      onTap: (){
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context)=> RecordViewWidget(curitem)));
-                      },
-                    );
-                  }
-            )
-        )
-    );
+        appBar: AppBar(title: Text('Flutter Engine')),
+        body: Center(
+            child: ListView.builder(
+                itemCount: this.items != null ? this.items.length : 0,
+                itemBuilder: (context, i) {
+                  final curitem = this.items[i];
+                  return ListTile(
+                    title: Text(curitem['participant_list']),
+                    subtitle: Text(curitem['staff_no']),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RecordViewWidget(curitem)));
+                    },
+                  );
+                })));
   }
 }
--- lib/recordview.dart
+++ lib/recordview.dart
@@ -1,51 +1,105 @@
 import 'package:flutter/material.dart';
 import './recordedit.dart';
+import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+var serverbase = '';
+
+final storage = new FlutterSecureStorage();
 
 class RecordViewWidget extends StatefulWidget {
-
   final item;
 
   RecordViewWidget(this.item);
 
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
+    print('item:' + this.item.toString());
     return RecordViewState(this.item);
   }
 }
 
 class RecordViewState extends State<RecordViewWidget> {
+  var _isLoading = true;
 
-  final item;
+  var _emailvalue = null;
+  var _passwordvalue = null;
 
-  RecordViewState(this.item);
+  var fields = [];
+  var item = {};
+
+  _setlocaldata() async {
+    print("setting localdata");
+    _emailvalue = await storage.read(key: 'email');
+    _passwordvalue = await storage.read(key: 'password');
+    serverbase = await storage.read(key: 'serverbase');
+    print("after localdata for $_emailvalue and $_passwordvalue");
+    _fetchData();
+  }
+
+  RecordViewState(this.item) {
+    print("init list");
+    _setlocaldata();
+  }
+
+  _fetchData() async {
+    print("in fetch for $_emailvalue and $_passwordvalue");
+    Dio dio = new Dio(new BaseOptions(headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization":
+          "Basic " + base64Encode(utf8.encode('$_emailvalue:$_passwordvalue'))
+    }));
+    final url = serverbase +
+        "/system/participants/jeager_2h_2918/" +
+        this.item['id'].toString();
+    print("requestting url:" + url);
+    final response = await dio.get(url);
+    print("Fetching data");
+
+    if (response.statusCode == 200) {
+      final map = response.data;
+      print("map:" + map.toString());
+      print("----------------------------------------------------------------");
+      print("record:" + map['record'].toString());
+      print("listfields:" + map['listfields'].toString());
+      //final assignmentsjson = map['record'];
+      setState(() {
+        _isLoading = false;
+        this.item = map['record'];
+        this.fields = map['listfields'];
+      });
+    }
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-                title: Text('Flutter Engine'),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: (){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context)=> RecordEditWidget(this.item)));
-                  },
-                )
-              ],
-            ),
-            body: ListView(
-              children: <Widget>[
-                ListTile(
-                  title: Text('Name'),
-                  subtitle: Text(this.item['name']),
-                ),
-                ListTile(
-                  title: Text('Description'),
-                  subtitle: Text(this.item['desc']),
-                )
-              ],
+        appBar: AppBar(
+          title: Text('Flutter Engine'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => RecordEditWidget(this.item)));
+              },
             )
-    );
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: this.fields != null ? this.fields.length : 0,
+            itemBuilder: (context, i) {
+              final curfield = this.fields[i];
+              print("processing:" + curfield.toString());
+              print("using:" + this.item.toString());
+              return ListTile(
+                title: Text(curfield['label']),
+                subtitle: Text(this.item[curfield['name']]['data'].toString()),
+              );
+            }));
   }
 }
